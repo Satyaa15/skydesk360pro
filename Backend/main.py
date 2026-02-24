1598133702a7eeefc9a036f22d29130681b89623
Satyanarayan Swamy <sswami3081@gmail.com>
2026-02-24 12:19:10 +0530

    Fix CORS middleware credentials setting

--- Backend/main.py
+++ Backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI(title=settings.PROJECT_NAME)
 app.add_middleware(
     CORSMiddleware,
     allow_origins=settings.CORS_ORIGINS,
-    allow_credentials="*" not in settings.CORS_ORIGINS,
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
